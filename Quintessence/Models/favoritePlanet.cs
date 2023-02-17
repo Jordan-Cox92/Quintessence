2060fe2ed0ac5b4f9c3fb434cbda3fd0226974c1
Jordan-Cox92! <mrjordancolbycox@gmail.com>
2023-02-16 21:59:15 -0500

    MVP branch

--- Quintessence/Models/favoritePlanet.cs
+++ Quintessence/Models/favoritePlanet.cs
@@ -4,13 +4,15 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Quintessence.Models
 {
-    public class favoritePlanet
+    public class FavoritePlanet
     {
         public int Id { get; set; }
 
         public int UserId { get; set; }
 
         public int PlanetId { get; set; }
+
+        public Planet Planet { get; set; }
 
        
 
